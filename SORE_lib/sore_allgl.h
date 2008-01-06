13ace087721ba02f24add8642d549b5ee212729d
Adam Kidder <thekidder@gmail.com>
2008-01-06 02:07:04 +0000

    Cleanup of SORE_allgl

--- SORE_lib/sore_allgl.h
+++ SORE_lib/sore_allgl.h
@@ -13,21 +13,10 @@
 #ifdef WIN32
 	#include <SDL.h>
 	#include <GL/wglew.h>
-	//wglGetProcAddress
-	
-	//#define uglGetProcAddress(x) wglGetProcAddress(x)
-	
-	//We need M_PI
 #else /* GLX */
 	#include <SDL/SDL.h>
-	//#include <SDL/SDL_image.h>
-	//#include <SDL/SDL_opengl.h>
 #include <GL/glxew.h>
-	
 	//Linux-specific to find system fonts
 	#include <fontconfig/fontconfig.h>
-	
-	//glXGetProcAddress
-	//#define uglGetProcAddress(x) (*glXGetProcAddress)((const GLubyte*)(x))
 #endif
 #endif //__ALLGL_H__
