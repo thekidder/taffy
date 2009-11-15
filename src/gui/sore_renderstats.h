ae98fa8c2996458efe2c6c5e727fe35bbb02ecd7
Adam Kidder <thekidder@gmail.com>
2009-11-14 20:30:41 -0500

    added renderstats to sore

---
+++ src/gui/sore_renderstats.h
@@ -0,0 +1,38 @@
+#ifndef RENDERSTATS_H
+#define RENDERSTATS_H
+
+#include "../sore_resource.h"
+
+namespace SORE_Graphics
+{
+    class Renderer2D;
+}
+
+namespace SORE_GUI
+{
+    class Widget;
+    class FrameWindow;
+    class TextWidget;
+
+    class RenderStats
+    {
+    public:
+        RenderStats(SORE_Resource::ResourcePool& pool, SORE_Graphics::Renderer2D* r,
+                       SORE_GUI::Widget* parent = 0);
+        ~RenderStats();
+
+        void SetVisible(bool visible = true);
+
+        void Frame(int elapsed);
+    private:
+        SORE_Graphics::Renderer2D* renderer;
+
+        SORE_GUI::FrameWindow* frame;
+        SORE_GUI::TextWidget* draws, *drawsLabel;
+        SORE_GUI::TextWidget* fps, *fpsLabel;
+        SORE_GUI::TextWidget* polys, *polysLabel;
+        SORE_GUI::TextWidget* ms, *msLabel;
+    };
+}
+
+#endif
