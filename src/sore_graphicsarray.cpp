91f529cdcd97213c600b6aa6d11673aa786a9049
Adam Kidder <thekidder@gmail.com>
2009-11-26 18:43:31 -0500

    formatting fixes

--- src/sore_graphicsarray.cpp
+++ src/sore_graphicsarray.cpp
@@ -21,124 +21,123 @@
 #include "sore_graphicsarray.h"
 #include "sore_logger.h"
 
-namespace SORE_Graphics
+SORE_Graphics::GraphicsArray::GraphicsArray(bool t, bool c, bool n) :
+    hasTexCoords(t), hasNormals(n), hasColors(c)
 {
-    GraphicsArray::GraphicsArray(bool t, bool c, bool n) :
-        hasTexCoords(t), hasNormals(n), hasColors(c)
+}
+
+void SORE_Graphics::GraphicsArray::Clear()
+{
+    vertices.clear();
+    indices.clear();
+}
+
+void SORE_Graphics::GraphicsArray::AddObject(
+    const GLfloat* v, const unsigned short* ind,
+    unsigned int numVertices, unsigned int numIndices,
+    const SORE_Math::Matrix4<float>* transform,
+    const GLfloat* t, const GLfloat* n, const GLfloat* c)
+{
+    if( (hasNormals && !n) || (hasTexCoords && !t) || (hasColors && !c) )
     {
+        ENGINE_LOG(SORE_Logging::LVL_ERROR,
+                   "Invalid VBO format; check constructer");
     }
+    size_t oldSize = vertices.size();
 
-    void GraphicsArray::Clear()
+    vertices.resize(vertices.size() + numVertices);
+    for(unsigned int i=0;i<numVertices;++i)
     {
-        vertices.clear();
-        indices.clear();
-    }
+        SORE_Math::Vector4<float> pos(v[i*3], v[i*3+1],v[i*3+2], 1.0f);
+        pos = *transform * pos;
 
-    void GraphicsArray::AddObject(const GLfloat* v, const unsigned short* ind,
-                                  unsigned int numVertices, unsigned int numIndices,
-                                  const SORE_Math::Matrix4<float>* transform,
-                                  const GLfloat* t, const GLfloat* n, const GLfloat* c)
-    {
-        if( (hasNormals && !n) || (hasTexCoords && !t) || (hasColors && !c) )
+        vertices[oldSize + i].x = pos[0];
+        vertices[oldSize + i].y = pos[1];
+        vertices[oldSize + i].z = pos[2];
+
+        if(hasColors)
         {
-            ENGINE_LOG(SORE_Logging::LVL_ERROR,
-                       "Invalid VBO format; check constructer");
+            vertices[oldSize + i].r = c[i*4];
+            vertices[oldSize + i].g = c[i*4+1];
+            vertices[oldSize + i].b = c[i*4+2];
+            vertices[oldSize + i].a = c[i*4+3];
         }
-        size_t oldSize = vertices.size();
-
-        vertices.resize(vertices.size() + numVertices);
-        for(unsigned int i=0;i<numVertices;++i)
-        {
-            SORE_Math::Vector4<float> pos(v[i*3], v[i*3+1],v[i*3+2], 1.0f);
-            pos = *transform * pos;
-
-            vertices[oldSize + i].x = pos[0];
-            vertices[oldSize + i].y = pos[1];
-            vertices[oldSize + i].z = pos[2];
-
-            if(hasColors)
-            {
-                vertices[oldSize + i].r = c[i*4];
-                vertices[oldSize + i].g = c[i*4+1];
-                vertices[oldSize + i].b = c[i*4+2];
-                vertices[oldSize + i].a = c[i*4+3];
-            }
-            if(hasTexCoords)
-            {
-                vertices[oldSize + i].texi = t[i*2+0];
-                vertices[oldSize + i].texj = t[i*2+1];
-            }
-            if(hasNormals)
-            {
-                vertices[oldSize + i].normx = n[i*3];
-                vertices[oldSize + i].normy = n[i*3+1];
-                vertices[oldSize + i].normz = n[i*3+2];
-            }
-        }
-
-        //copy vertices into VBO, taking into account index renumbering
-        for(unsigned short j=0;j<numIndices;++j)
-        {
-            unsigned short index = ind[j] + oldSize;
-            indices.push_back(index);
-        }
-    }
-
-    void GraphicsArray::BeginDraw()
-    {
-        BeginDrawHook();
-
-        glVertexPointer(3, GL_FLOAT, sizeof(vertex), GetOffset(&(vertices[0]), 0));
         if(hasTexCoords)
         {
-            glClientActiveTexture(GL_TEXTURE0);
-            glTexCoordPointer(2, GL_FLOAT, sizeof(vertex),
-                              GetOffset(&(vertices[0]), 12));
+            vertices[oldSize + i].texi = t[i*2+0];
+            vertices[oldSize + i].texj = t[i*2+1];
         }
         if(hasNormals)
         {
-            glNormalPointer(GL_FLOAT, sizeof(vertex), GetOffset(&(vertices[0]), 20));
+            vertices[oldSize + i].normx = n[i*3];
+            vertices[oldSize + i].normy = n[i*3+1];
+            vertices[oldSize + i].normz = n[i*3+2];
         }
-        if(hasColors)
-        {
-            glColorPointer(4, GL_FLOAT, sizeof(vertex), GetOffset(&(vertices[0]), 32));
-        }
-
-        glEnableClientState(GL_VERTEX_ARRAY);
-        if(hasColors)
-          glEnableClientState(GL_COLOR_ARRAY);
-        if(hasTexCoords)
-            glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-        if(hasNormals)
-            glEnableClientState(GL_NORMAL_ARRAY);
-
     }
 
-    void GraphicsArray::EndDraw()
+    //copy vertices into VBO, taking into account index renumbering
+    for(unsigned short j=0;j<numIndices;++j)
     {
-        if(hasColors)
-            glDisableClientState(GL_COLOR_ARRAY);
-        if(hasNormals)
-            glDisableClientState(GL_NORMAL_ARRAY);
-        if(hasTexCoords)
-            glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-        glDisableClientState(GL_VERTEX_ARRAY);
+        unsigned short index = ind[j] + oldSize;
+        indices.push_back(index);
+    }
+}
+
+void SORE_Graphics::GraphicsArray::BeginDraw()
+{
+    BeginDrawHook();
+
+    glVertexPointer(3, GL_FLOAT, sizeof(vertex), GetOffset(&(vertices[0]), 0));
+    if(hasTexCoords)
+    {
+        glClientActiveTexture(GL_TEXTURE0);
+        glTexCoordPointer(2, GL_FLOAT, sizeof(vertex),
+                          GetOffset(&(vertices[0]), 12));
+    }
+    if(hasNormals)
+    {
+        glNormalPointer(GL_FLOAT, sizeof(vertex), GetOffset(&(vertices[0]), 20));
+    }
+    if(hasColors)
+    {
+        glColorPointer(4, GL_FLOAT, sizeof(vertex), GetOffset(&(vertices[0]), 32));
     }
 
-    void GraphicsArray::DrawAll()
-    {
-        glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_SHORT,
-                       GetOffset(&(indices[0]), 0));
-    }
+    glEnableClientState(GL_VERTEX_ARRAY);
+    if(hasColors)
+        glEnableClientState(GL_COLOR_ARRAY);
+    if(hasTexCoords)
+        glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+    if(hasNormals)
+        glEnableClientState(GL_NORMAL_ARRAY);
 
-    void GraphicsArray::DrawElements(unsigned int numTris, unsigned short triOffset)
-    {
-        glDrawElements(GL_TRIANGLES, numTris*3, GL_UNSIGNED_SHORT,
-                       GetOffset(&(indices[0]), triOffset*3*sizeof(unsigned short)));
-    }
+}
 
-    bool GraphicsArray::Empty() const
-    {
-        return indices.empty();
-    }
+void SORE_Graphics::GraphicsArray::EndDraw()
+{
+    if(hasColors)
+        glDisableClientState(GL_COLOR_ARRAY);
+    if(hasNormals)
+        glDisableClientState(GL_NORMAL_ARRAY);
+    if(hasTexCoords)
+        glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+    glDisableClientState(GL_VERTEX_ARRAY);
+}
+
+void SORE_Graphics::GraphicsArray::DrawAll()
+{
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_SHORT,
+                   GetOffset(&(indices[0]), 0));
+}
+
+void SORE_Graphics::GraphicsArray::DrawElements(
+    unsigned int numTris, unsigned short triOffset)
+{
+    glDrawElements(GL_TRIANGLES, numTris*3, GL_UNSIGNED_SHORT,
+                   GetOffset(&(indices[0]), triOffset*3*sizeof(unsigned short)));
+}
+
+bool SORE_Graphics::GraphicsArray::Empty() const
+{
+    return indices.empty();
 }
