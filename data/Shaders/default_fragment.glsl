c7ca246f9c79c7a55019edb5f133d856b364098c
Adam Kidder <thekidder@gmail.com>
2009-11-28 17:49:09 -0500

    changed sample name to fit new renderer architecture

--- data/Shaders/default_fragment.glsl
+++ data/Shaders/default_fragment.glsl
@@ -1,10 +1,8 @@
 varying vec4 color;
-uniform sampler2D tex;
+uniform sampler2D texture;
 
 void main()
 {
-	
-	vec4 texel = texture2D(tex, gl_TexCoord[0].st).rgba;
-	
+	vec4 texel = texture2D(texture, gl_TexCoord[0].st).rgba;
 	gl_FragColor = color*texel;
 }
