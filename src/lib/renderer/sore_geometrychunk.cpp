af943c2f6904ac4b20a5a4ca6651157c6ad0bb9f
Adam Kidder <thekidder@gmail.com>
2010-05-05 00:17:30 -0400

    fixed indentation

--- src/lib/renderer/sore_geometrychunk.cpp
+++ src/lib/renderer/sore_geometrychunk.cpp
@@ -1,36 +1,36 @@
 /**************************************************************************
- * Copyright 2010 Adam Kidder. All rights reserved.                       *
- *                                                                        *
- * Redistribution and use in source and binary forms, with or without     *
- * modification, are permitted provided that the following conditions     *
- * are met:                                                               *
- *                                                                        *
- *    1. Redistributions of source code must retain the above             *
- *       copyright notice, this list of conditions and the following      *
- *       disclaimer.                                                      *
- *                                                                        *
- *    2. Redistributions in binary form must reproduce the above          *
- *       copyright notice, this list of conditions and the following      *
- *       disclaimer in the documentation and/or other materials           *
- *       provided with the distribution.                                  *
- *                                                                        *
- * THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND ANY      *
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE      *
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR     *
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> OR        *
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,  *
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,    *
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR     *
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY    *
- * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT           *
- * (INCLUDING ENGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  *
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   *
- *                                                                        *
- * The views and conclusions contained in the software and documentation  *
- * are those of the authors and should not be interpreted as              *
- * representing official policies, either expressed or implied, of        *
- * Adam Kidder.                                                           *
- **************************************************************************/
+* Copyright 2010 Adam Kidder. All rights reserved.                       *
+*                                                                        *
+* Redistribution and use in source and binary forms, with or without     *
+* modification, are permitted provided that the following conditions     *
+* are met:                                                               *
+*                                                                        *
+*    1. Redistributions of source code must retain the above             *
+*       copyright notice, this list of conditions and the following      *
+*       disclaimer.                                                      *
+*                                                                        *
+*    2. Redistributions in binary form must reproduce the above          *
+*       copyright notice, this list of conditions and the following      *
+*       disclaimer in the documentation and/or other materials           *
+*       provided with the distribution.                                  *
+*                                                                        *
+* THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND ANY      *
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE      *
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR     *
+* PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> OR        *
+* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,  *
+* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,    *
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR     *
+* PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY    *
+* OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT           *
+* (INCLUDING ENGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  *
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   *
+*                                                                        *
+* The views and conclusions contained in the software and documentation  *
+* are those of the authors and should not be interpreted as              *
+* representing official policies, either expressed or implied, of        *
+* Adam Kidder.                                                           *
+**************************************************************************/
 
 #include <cstring>
 #include <limits>
@@ -42,14 +42,14 @@ SORE_Graphics::GeometryChunk::GeometryChunk(
     unsigned int nVertices, unsigned int nIndices, GLenum type)
     : numVertices(nVertices), numIndices(nIndices), type(type)
 {
-	if(nIndices > std::numeric_limits<unsigned short>::max())
-		ENGINE_LOG(SORE_Logging::LVL_ERROR, "Too many indices in geometry chunk");
-	if(nVertices > std::numeric_limits<unsigned short>::max())
-		ENGINE_LOG(SORE_Logging::LVL_ERROR, "Too many vertices in geometry chunk");
-	if((type == GL_TRIANGLES && nIndices % 3 != 0) ||
-       (type == GL_LINES     && nIndices % 2 != 0) ||
-       (type == GL_QUADS     && nIndices % 4 != 0))
-		ENGINE_LOG(SORE_Logging::LVL_ERROR, "Number of indices is invalid");
+    if(nIndices > std::numeric_limits<unsigned short>::max())
+        ENGINE_LOG(SORE_Logging::LVL_ERROR, "Too many indices in geometry chunk");
+    if(nVertices > std::numeric_limits<unsigned short>::max())
+        ENGINE_LOG(SORE_Logging::LVL_ERROR, "Too many vertices in geometry chunk");
+    if((type == GL_TRIANGLES && nIndices % 3 != 0) ||
+        (type == GL_LINES     && nIndices % 2 != 0) ||
+        (type == GL_QUADS     && nIndices % 4 != 0))
+        ENGINE_LOG(SORE_Logging::LVL_ERROR, "Number of indices is invalid");
     data = new vertex[numVertices];
     indices = new unsigned short[numIndices];
 }
