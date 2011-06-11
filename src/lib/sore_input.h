e8f2b9b44f0c5ddbddd7a68830900bce4ebc0846
Adam Kidder <thekidder@gmail.com>
2011-06-11 13:46:47 -0400

    changed event type to enum

--- src/lib/sore_input.h
+++ src/lib/sore_input.h
@@ -53,114 +53,117 @@
 
 namespace SORE_Kernel
 {
-	const unsigned int NOEVENT         = 0x000;
-	const unsigned int MOUSEMOVE       = 0x001;
-	const unsigned int MOUSEBUTTONDOWN = 0x002;
-	const unsigned int MOUSEBUTTONUP   = 0x004;
-	const unsigned int MOUSECLICK      = 0x008;
-	const unsigned int KEYDOWN         = 0x010;
-	const unsigned int KEYUP           = 0x020;
-	const unsigned int KEYCLICK        = 0x040;
+    enum Event_type_t
+    {
+        NOEVENT         = 0x000,
+        MOUSEMOVE       = 0x001,
+        MOUSEBUTTONDOWN = 0x002,
+        MOUSEBUTTONUP   = 0x004,
+        MOUSECLICK      = 0x008,
+        KEYDOWN         = 0x010,
+        KEYUP           = 0x020,
+        KEYCLICK        = 0x040,
 
-	//used for SORE_GUI
-	const unsigned int MOUSEENTER      = 0x080;
-	const unsigned int MOUSELEAVE      = 0x100;
-	
-	//window resize is called when the user physically resizes the window
-	//this really should not be called by any external functions
-	const unsigned int WINDOWRESIZE    = 0x200;
-	//resize is called when the actual screen is resized by *any* method
-	const unsigned int RESIZE          = 0x400;
-	
-	const unsigned int MOUSE_BUTTON1   = 0x01;
-	const unsigned int MOUSE_BUTTON2   = 0x02;
-	const unsigned int MOUSE_BUTTON3   = 0x04;
-	const unsigned int MOUSE_WHEELDOWN = 0x08;
-	const unsigned int MOUSE_WHEELUP   = 0x10;
+        //used for SORE_GUI
+        MOUSEENTER      = 0x080,
+        MOUSELEAVE      = 0x100,
+    
+        //window resize is called when the user physically resizes the window
+        //this really should not be called by any external functions
+        WINDOWRESIZE    = 0x200,
+        //resize is called when the actual screen is resized by *any* method
+        RESIZE          = 0x400,
+    
+        MOUSE_BUTTON1   = 0x01,
+        MOUSE_BUTTON2   = 0x02,
+        MOUSE_BUTTON3   = 0x04,
+        MOUSE_WHEELDOWN = 0x08,
+        MOUSE_WHEELUP   = 0x10,
 
-	const unsigned int INPUT_ALL       = MOUSEMOVE | MOUSEBUTTONDOWN | MOUSEBUTTONUP 
-		| MOUSECLICK | KEYDOWN | KEYUP | KEYCLICK | MOUSEENTER | MOUSELEAVE;
+        INPUT_ALL       = MOUSEMOVE | MOUSEBUTTONDOWN | MOUSEBUTTONUP 
+        | MOUSECLICK | KEYDOWN | KEYUP | KEYCLICK | MOUSEENTER | MOUSELEAVE,
 
-	const unsigned int INPUT_ALLMOUSE  = MOUSEMOVE | MOUSEBUTTONDOWN | MOUSEBUTTONUP | MOUSECLICK | MOUSEENTER | MOUSELEAVE;
-	
-	struct MouseInfo
-	{
-		unsigned int x,y;
-		int          xmove,ymove;
-		unsigned int buttonState;
-	};
-	
-	struct KeyInfo
-	{
-		unsigned int keySym;
-		unsigned int modState;
-		unsigned int unicode;
-	};
-	
-	struct ResizeInfo
-	{
-		int w,h;
-	};
-	
-	struct Event
-	{
-		unsigned int type;
-		KeyInfo key;
-		MouseInfo mouse;
-		ResizeInfo resize;
-	};
-	
-	typedef std::vector<std::pair<unsigned int, boost::function<bool (Event*)> > > event_map;
+        INPUT_ALLMOUSE  = MOUSEMOVE | MOUSEBUTTONDOWN | MOUSEBUTTONUP | MOUSECLICK | MOUSEENTER | MOUSELEAVE
+    };
+    
+    struct MouseInfo
+    {
+        unsigned int x,y;
+        int          xmove,ymove;
+        unsigned int buttonState;
+    };
+    
+    struct KeyInfo
+    {
+        unsigned int keySym;
+        unsigned int modState;
+        unsigned int unicode;
+    };
+    
+    struct ResizeInfo
+    {
+        int w,h;
+    };
+    
+    struct Event
+    {
+        Event_type_t type;
+        KeyInfo key;
+        MouseInfo mouse;
+        ResizeInfo resize;
+    };
+    
+    typedef std::vector<std::pair<unsigned int, boost::function<bool (Event*)> > > event_map;
 
-	class SORE_EXPORT InputTask;
+    class SORE_EXPORT InputTask;
 
-	struct event_listener_ref
-	{
-	private:
-		std::vector<event_map>::iterator stackPos;
-		event_map::iterator event;
-		friend class SORE_EXPORT InputTask;
-	};
-	
-	class SORE_EXPORT InputTask : public Task
-	{
-	public:
-		InputTask();
-		~InputTask();
-			
-		void Frame(int elapsedTime);
-		void Pause();
-		void Resume();
-			
-		const char* GetName() const {return "Input task";}
-			
-		event_listener_ref AddListener(unsigned int eventType, boost::function<bool (Event*)> functor);
-		void RemoveListener(event_listener_ref listener);
-		void InjectEvent(SORE_Kernel::Event e);
+    struct event_listener_ref
+    {
+    private:
+        std::vector<event_map>::iterator stackPos;
+        event_map::iterator event;
+        friend class SORE_EXPORT InputTask;
+    };
+    
+    class SORE_EXPORT InputTask : public Task
+    {
+    public:
+        InputTask();
+        ~InputTask();
+            
+        void Frame(int elapsedTime);
+        void Pause();
+        void Resume();
+            
+        const char* GetName() const {return "Input task";}
+            
+        event_listener_ref AddListener(unsigned int eventType, boost::function<bool (Event*)> functor);
+        void RemoveListener(event_listener_ref listener);
+        void InjectEvent(SORE_Kernel::Event e);
 
-		bool QuitEventReceived() const;
+        bool QuitEventReceived() const;
 
-		//This "state" is used by Gamestate. Only input handlers in the current state are respected 
-		//(with Resize events as an exception)
-		void PushState();
-		void PopState();
-	private:
-		//translate the given event and place it into event
-		void TranslateEvent(SDL_Event& sdl_event);
-		//processes the member variable event
-		void HandleEvent(Event& event);
+        //This "state" is used by Gamestate. Only input handlers in the current state are respected 
+        //(with Resize events as an exception)
+        void PushState();
+        void PopState();
+    private:
+        //translate the given event and place it into event
+        void TranslateEvent(SDL_Event& sdl_event);
+        //processes the member variable event
+        void HandleEvent(Event& event);
 
-		std::vector<
-			event_map
-			> allListeners;
-		//reference to the current listener list
-		std::vector<event_map>::iterator currentListeners;
+        std::vector<
+            event_map
+            > allListeners;
+        //reference to the current listener list
+        std::vector<event_map>::iterator currentListeners;
 
-		Event event;
-		std::queue<Event> injectedEvents;
+        Event event;
+        std::queue<Event> injectedEvents;
 
-		bool quitEvent;
-	};
+        bool quitEvent;
+    };
 }
 
 #ifdef _MSC_VER
