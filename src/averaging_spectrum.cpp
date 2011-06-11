648df1c9c0da2425c66ead8b63a9928098fe0a89
Adam Kidder <thekidder@gmail.com>
2011-06-11 14:39:43 -0400

    correct bucket hz range output in averaging spectrum

--- src/averaging_spectrum.cpp
+++ src/averaging_spectrum.cpp
@@ -38,13 +38,10 @@ const SpectrumSnapshot& AveragingSpectrum::Snapshot() const
 
 std::pair<float, float> AveragingSpectrum::HzRange(size_t bucket) const
 {
-    // assume source spectrum is linear
-    float hz_per_src_bucket = Source().HzRange(0).second - Source().HzRange(0).first;
-
     size_t min_bucket = bucket_src_ranges[bucket].first;
-    size_t max_bucket = bucket_src_ranges[bucket].first;
+    size_t max_bucket = bucket_src_ranges[bucket].second;
 
-    return std::make_pair(min_bucket * hz_per_src_bucket, (max_bucket + 1) * hz_per_src_bucket);
+    return std::make_pair(Source().HzRange(min_bucket).first, Source().HzRange(max_bucket).second);
 }
 
 const Spectrum& AveragingSpectrum::Source() const
